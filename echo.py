62882cf04dbf0c7f879875f784ee70cac183f597
Henrique Simões <henrique.m.simoes@tecnico.ulisboa.pt>
2024-05-07 19:17:37 +0100

    echo_version2 created

--- echo.py
+++ echo.py
@@ -9,23 +9,24 @@ can also be used if you dont want to create a full node but still want to intera
 
 
 
+
+import matplotlib.pyplot as plt
+import numpy as np
 import rospy
 import time
 from common_msgs.msg import ConeDetections, Cone
 from nav_msgs.msg import Path
 
-class Echo :
+
 
     def __init__(self, listen_topic, publish_topic, message_type):
         self.listen_topic = listen_topic
-        self.publish_topic = publish_topic
         self.message_type = message_type
 
         self.listen_mode = False
         self.loaded_message = False
         
         self.subTopic()
-        self.advertTopic()
         self.loop()
 
     def subTopic(self):
@@ -35,22 +36,12 @@ class Echo :
         print("with type: ", self.message_type)
         print()
 
-    def advertTopic(self):
-        # name of the topic that you will want to publish
-        self.my_publsiher = rospy.Publisher(self.publish_topic, self.message_type, queue_size=1)
-        print("Publishing to topic: ", self.publish_topic)
-        print("with type: ", self.message_type)
-        print()
-
     def messageCallback(self, msg):
 
         if self.listen_mode:
-            print("have saved a new message")
+            print("have received a new message")
             self.loaded_message = msg
 
-    def publishMessage(self):
-        self.my_publsiher.publish(self.loaded_message)
-        
     def loop(self):
         # start ui loop
         while True:
@@ -78,19 +69,62 @@ class Echo :
                 break
 
 
+#Function that does a x and y plot of the pose with a arrow that points to the direction of the robot
+def plot_robot_pose(Sub_topic prediction):
+    """
+    Plot the pose of a robot with an arrow indicating its direction.
+
+    Parameters:
+        x (float): x-coordinate of the robot's pose.
+        y (float): y-coordinate of the robot's pose.
+        theta (float): orientation of the robot in radians (0 is pointing along the x-axis).
+    """
+    x = prediction.loaded_message.poses[0].pose.position.x
+    y = prediction.loaded_message.poses[0].pose.position.y
+    theta = prediction.loaded_message.poses[0].pose.orientation.z
+    # Set up the figure and axis
+    fig, ax = plt.subplots()
+
+    # Plot the robot position
+    ax.plot(x, y, 'ro')
+
+    # Calculate the end point of the arrow
+    arrow_length = 0.5
+    dx = arrow_length * np.cos(theta)
+    dy = arrow_length * np.sin(theta)
+
+    # Plot the arrow
+    ax.arrow(x, y, dx, dy, head_width=0.1, head_length=0.1, fc='k', ec='k')
+
+    # Set equal aspect ratio
+    ax.set_aspect('equal', adjustable='box')
+
+    # Set labels and title
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_title('Robot Pose')
+
+    # Show the plot
+    plt.show()
+    
+
 
 def main():
 
     # Variables to CHANGE according to your need
-    listen_topic = "/control/path_planner/centerline"
-    publish_topic = "/control/path_planner/centerline"
+    listen_topic1 = "/scan"
+    listen_topic2 = "/pose"
+    publish_topic = "/localization"
     message_type = Path
 
 
-    rospy.init_node('echo_node')
+    rospy.init_node('localization_node')
 
     # creat the main class
-    my_echo = Echo(listen_topic, publish_topic, message_type)
+    prediction = Sub_topic(listen_topic1, publish_topic, message_type)
+    plot_robot_pose(prediction)
+    observation = Sub_topic(listen_topic2, publish_topic, message_type)
+    
 
 
 if __name__ == "__main__":
